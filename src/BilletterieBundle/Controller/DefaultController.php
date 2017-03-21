18c0eeeefb1b388fa6bf9965cc6bb1c94ee5c4fe
samboy45 <170145@supinfo.com>
2017-03-21 14:56:59 +0100

    Routing

--- src/BilletterieBundle/Controller/DefaultController.php
+++ src/BilletterieBundle/Controller/DefaultController.php
@@ -8,10 +8,50 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
-        return $this->render('BilletterieBundle:Default:index.html.twig');
+        return $this->render('BilletterieBundle:Order:index.html.twig');
+    }
+
+    /**
+     * @Route("/validation", name="validation")
+     */
+    public function validationAction()
+    {
+        return $this->render('BilletterieBundle:Order:validation.html.twig');
+    }
+
+    /**
+     * @Route("/confirmation", name="confirmation")
+     */
+    public function confirmationAction()
+    {
+        return $this->render('BilletterieBundle:Order:confirmation.html.twig');
+    }
+
+    /**
+     * @Route("/info", name="info")
+     */
+    public function infoAction()
+    {
+        return $this->render('BilletterieBundle:Info:info.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('BilletterieBundle:Info:contact.html.twig');
+    }
+
+    /**
+     * @Route("/cgv", name="cgv")
+     */
+    public function cgvAction()
+    {
+        return $this->render('BilletterieBundle:Info:cgv.html.twig');
     }
 }
